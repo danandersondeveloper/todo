4147081fef245d9c7bdffe0dfddf36e00c40a889
dananderson <blusplatt@gmail.com>
2024-01-17 19:33:04 +0200

    Start implementing the main contant

--- script/main.js
+++ script/main.js
@@ -1,12 +1,27 @@
 
 const main_menu = document.querySelector('#main-menu');
 const main_menu_close_button = main_menu.querySelector('.close')
+const add_new_task_button = document.querySelector('#add-new-task');
+const new_task_element = document.querySelector('#new-task')
 
 
 const toggleMainMenu = () => {
-	main_menu.classList.toggle("hide")
+	main_menu.classList.toggle('hide');
 }
 
+const toggleNewTask = () => {
+	new_task_element.classList.toggle('show');
+}
+
+const hide_add_new_task_button = () => {
+	add_new_task_button.classList.toggle('hide')
+}
+
+add_new_task_button.addEventListener('click', () => {
+	toggleNewTask();
+	hide_add_new_task_button();
+});
+
 main_menu_close_button.addEventListener('click', () => {
-	toggleMainMenu()
+	toggleMainMenu();
 });
